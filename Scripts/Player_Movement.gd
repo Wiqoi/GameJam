4691879e09507bdd6f1c2bb02d50c3120d471aa4
Wiqoi <szjasonhuang@outlook.com>
2025-05-15 21:36:59 +0800

    Initial commit

---
+++ Scripts/Player_Movement.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@export var movement_speed : float = 500
+@export var deceleration : float = 1000
+var character_direction : Vector2
+
+func _physics_process(delta):
+	character_direction.x = Input.get_axis("Player_Left", "Player_Right")
+	character_direction.y = Input.get_axis("Player_Up", "Player_Down")
+	
+	character_direction = character_direction.normalized()
+	
+	if character_direction.x > 0:
+		$Sprite.flip_h = false
+	elif character_direction.x < 0:
+		$Sprite.flip_h = true
+	
+	if character_direction:
+		velocity = character_direction * movement_speed
+		if abs(character_direction.y) > abs(character_direction.x):
+			if character_direction.y < 0:
+				if $Sprite.animation != "WalkingUp":
+					$Sprite.animation = "WalkingUp"
+			else:
+				if $Sprite.animation != "WalkingDown":
+					$Sprite.animation = "WalkingDown"
+		else:
+			if $Sprite.animation != "Walking":
+				$Sprite.animation = "Walking"
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration)
+		if $Sprite.animation == "WalkingUp":
+			$Sprite.animation = "IdleUp"
+		if $Sprite.animation == "WalkingDown":
+			$Sprite.animation = "IdleDown"
+		else:
+			$Sprite.animation = "IdleDown"
+	
+	move_and_slide()
