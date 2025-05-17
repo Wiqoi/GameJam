0f62df07ef3d86e810dce1cf019f385fea981446
Wiqoi <szjasonhuang@outlook.com>
2025-05-17 12:31:10 +0800

    Removed useless clutter

--- Scripts/Enemy_Spawn.gd
+++ Scripts/Enemy_Spawn.gd
@@ -4,7 +4,7 @@ func _ready():
 	pass
 
 func spawn_enemy():
-	var new_enemy = preload("res://Scenes/enemy.tscn").instantiate()
+	var new_enemy = preload("res://Scenes/Enemies/enemy.tscn").instantiate()
 	new_enemy.player = get_node("/root/Game/Player")
 	%SpawnPathFollow.progress_ratio = randf()
 	new_enemy.global_position = %SpawnPathFollow.global_position
