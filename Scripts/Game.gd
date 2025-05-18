9f50d01a1ce9b6c072e36b07702a48d416a087e0
BraydenLuo <braydenluo@163.com>
2025-05-18 21:33:48 +0800

    add demon, changing some names

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
+var randomEnemy
+
 func _ready():
 	pass
 
 func spawn_enemy():
-	var new_enemy = preload("res://Scenes/Enemies/enemy.tscn").instantiate()
-	new_enemy.player = get_node("/root/Game/Player")
+	var new_slime = preload("res://Scenes/Enemies/slime.tscn").instantiate()
+	var new_demon = preload("res://Scenes/Enemies/demon.tscn").instantiate()
+	new_slime.player = get_node("/root/Game/Player")
+	new_demon.player = get_node("/root/Game/Player")
 	%SpawnPathFollow.progress_ratio = randf()
-	new_enemy.global_position = %SpawnPathFollow.global_position
-	add_child(new_enemy)
+	randomEnemy = randf()
+	if randomEnemy > 0.5:
+		new_slime.global_position = %SpawnPathFollow.global_position
+		add_child(new_slime)
+	else:
+		new_demon.global_position = %SpawnPathFollow.global_position
+		add_child(new_demon)
 	
 func _on_timer_timeout() -> void:
 	spawn_enemy()
