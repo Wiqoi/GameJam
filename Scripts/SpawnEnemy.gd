8cdd6e61e3fa267e20a59562033884d568a28f4f
BraydenLuo <braydenluo@163.com>
2025-05-19 21:31:34 +0800

    waves

--- Scripts/SpawnEnemy.gd
+++ Scripts/SpawnEnemy.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
 var randomEnemy
+var waveNum : int = 0
+var timeCount : int = 0
 
 func _ready():
 	pass
 
-func spawn_enemy():
+func spawn_enemy1():
+	var new_slime = preload("res://Scenes/Enemies/slime.tscn").instantiate()
+	new_slime.player = get_node("/root/Game/Player")
+	%SpawnPathFollow.progress_ratio = randf()
+	new_slime.global_position = %SpawnPathFollow.global_position
+	add_child(new_slime)
+		
+func spawn_enemy2():
+	var new_demon = preload("res://Scenes/Enemies/demon.tscn").instantiate()
+	new_demon.player = get_node("/root/Game/Player")
+	%SpawnPathFollow.progress_ratio = randf()
+	new_demon.global_position = %SpawnPathFollow.global_position
+	add_child(new_demon)
+		
+func spawn_enemy3():
 	var new_slime = preload("res://Scenes/Enemies/slime.tscn").instantiate()
 	var new_demon = preload("res://Scenes/Enemies/demon.tscn").instantiate()
 	new_slime.player = get_node("/root/Game/Player")
@@ -21,4 +37,13 @@ func spawn_enemy():
 	
 
 func _on_timer_timeout() -> void:
-	spawn_enemy()
+	timeCount += 1
+
+
+func _on_spawn_enemy_timer_timeout() -> void:
+	if timeCount < 30:
+		spawn_enemy1()
+	elif 50 < timeCount and timeCount < 80:
+		spawn_enemy2()
+	elif timeCount > 100:
+		spawn_enemy3()
