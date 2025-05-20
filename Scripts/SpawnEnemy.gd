001bc82776a742562849543a3085194ea5936f84
BraydenLuo <braydenluo@163.com>
2025-05-20 20:00:32 +0800

    add and adjust phases

--- Scripts/SpawnEnemy.gd
+++ Scripts/SpawnEnemy.gd
@@ -42,6 +42,25 @@ func spawn_enemy4():
 	new_morph.global_position = %SpawnPathFollow.global_position
 	add_child(new_morph)
 	
+func spawn_enemy5():
+	var new_slime = preload("res://Scenes/Enemies/slime.tscn").instantiate()
+	var new_demon = preload("res://Scenes/Enemies/demon.tscn").instantiate()
+	var new_morph = preload("res://Scenes/Enemies/morph.tscn").instantiate()
+	new_morph.player = get_node("/root/Game/Player")
+	new_slime.player = get_node("/root/Game/Player")
+	new_demon.player = get_node("/root/Game/Player")
+	%SpawnPathFollow.progress_ratio = randf()
+	randomEnemy = randf()
+	if randomEnemy > 0.66:
+		new_slime.global_position = %SpawnPathFollow.global_position
+		add_child(new_slime)
+	elif randomEnemy > 0.33:
+		new_demon.global_position = %SpawnPathFollow.global_position
+		add_child(new_demon)
+	else:
+		new_morph.global_position = %SpawnPathFollow.global_position
+		add_child(new_morph)
+	
 
 func _on_timer_timeout() -> void:
 	timeCount += 1
@@ -52,7 +71,9 @@ func _on_spawn_enemy_timer_timeout() -> void:
 		spawn_enemy1()
 	elif 50 < timeCount and timeCount < 80:
 		spawn_enemy2()
-	elif timeCount > 100 and timeCount < 130:
+	elif timeCount > 90 and timeCount < 120:
 		spawn_enemy3()
-	elif timeCount > 150:
+	elif timeCount > 135 and timeCount < 165:
 		spawn_enemy4()
+	elif timeCount > 180 and timeCount < 210:
+		spawn_enemy5()
