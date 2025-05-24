396bf77bf8526fbc6b6eb28922a2419042e68a28
BraydenLuo <braydenluo@163.com>
2025-05-24 09:21:12 +0800

    change =UI

--- Scripts/StartingInterface.gd
+++ Scripts/StartingInterface.gd
@@ -5,3 +5,9 @@ func _on_start_game_pressed() -> void:
 	var err = get_tree().change_scene_to_file("res://Scenes/Levels/game.tscn")
 	if err != OK:
 		push_error("Fail:", err)
+		
+
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
