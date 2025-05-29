aa8a30c3fa5c0629a69f732c77d9636d69934d68
Wiqoi <szjasonhuang@outlook.com>
2025-05-29 13:59:19 +0800

    UIHealth

--- Scenes/upgradeScreen.gd
+++ Scenes/upgradeScreen.gd
@@ -1,8 +1,13 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	Global.playerHP += 1
+	Global.playerMaxHP += 1
+	Global.playerHp += 1
 	Global.playerDmg += 1
+	$Button.visible = false
+	$Button2.visible = false
 
 func _on_button_2_pressed() -> void:
 	Global.currentPlayerAbility = 0
+	$Button.visible = false
+	$Button2.visible = false
