e1688292ca7e68de3e409763948a440ebcd5d102
BraydenLuo <152962602+Braydenluo@users.noreply.github.com>
2025-05-30 14:25:09 +0800

    finally working

--- Scripts/upgradeScreen.gd
+++ Scripts/upgradeScreen.gd
@@ -5,6 +5,16 @@ func _on_button_pressed() -> void:
 	Global.playerHP = Global.playerMaxHP
 	Global.playerSkill = Global.playerMaxHP
 	Global.playerDmg += 1
+	%HealthBar1.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar2.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar3.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar4.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar5.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar6.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar7.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar8.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar9.texture = load("res://Assets/OurAssets/UI/Health2.png")
+	%HealthBar10.texture = load("res://Assets/OurAssets/UI/Health2.png")
 	$Button.visible = false
 	$Button2.visible = false
 	Global.betweenPhases = false
